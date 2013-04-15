dd9551935d85f5fdd2a98fa0f10c0ddf012341a9
Holger Muegge <muegge@acm.org>
2013-04-15 20:22:49 +0200

    Introduced @WithAssets annotation to specifiy assets dir for app under test

--- GQ_Android/test/com/qeevee/gq/tests/robolectric/GQTestRunner.java
+++ GQ_Android/test/com/qeevee/gq/tests/robolectric/GQTestRunner.java
@@ -8,6 +8,17 @@ import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricConfig;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 
+/**
+ * <li>Uses manifest and resources from the GQ_Android project.
+ * 
+ * <li>You can use @WithAssets annotation in test class to specify the location
+ * of the assets directory.
+ * 
+ * <li>Binds shadow class for Bitmaps
+ * 
+ * @author muegge
+ * 
+ */
 public class GQTestRunner extends RobolectricTestRunner {
 
 	public static final File MANIFEST_FILE = new File(
@@ -17,7 +28,24 @@ public class GQTestRunner extends RobolectricTestRunner {
 
 	public GQTestRunner(Class<?> testClass) throws InitializationError {
 		super(testClass, new RobolectricConfig(MANIFEST_FILE, RESOURCE_DIR,
-				ASSETS_DIR));
+				getAssetsDir(testClass)));
+	}
+
+	private static File getAssetsDir(Class<?> testClass) {
+		WithAssets wmd = testClass.getAnnotation(WithAssets.class);
+		if (wmd == null)
+			return ASSETS_DIR;
+		else {
+			File assetsDir = new File(wmd.value());
+			if (assetsDir.exists() && assetsDir.canRead())
+				return assetsDir;
+			else
+				throw new Error(
+						"Test class "
+								+ testClass.getSimpleName()
+								+ " uses WithAssets annotation with a non readable directory \""
+								+ wmd.value() + "\".");
+		}
 	}
 
 	@Override
