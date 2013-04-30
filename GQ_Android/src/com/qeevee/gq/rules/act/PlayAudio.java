3ac67587753179baf810ffd23098db4ef46d7377
Holger Muegge <muegge@acm.org>
2013-04-30 14:00:25 +0200

    format

--- GQ_Android/src/com/qeevee/gq/rules/act/PlayAudio.java
+++ GQ_Android/src/com/qeevee/gq/rules/act/PlayAudio.java
@@ -4,16 +4,15 @@ import edu.bonn.mobilegaming.geoquest.GeoQuestApp;
 
 public class PlayAudio extends Action {
 
-    @Override
-    protected boolean checkInitialization() {
-	return (params.containsKey("file"));
-    }
+	@Override
+	protected boolean checkInitialization() {
+		return (params.containsKey("file"));
+	}
 
-    @Override
-    public void execute() {
-	if (params.containsKey("file"))
-	    GeoQuestApp.playAudio(params.get("file"),
-				  false);
-    }
+	@Override
+	public void execute() {
+		if (params.containsKey("file"))
+			GeoQuestApp.playAudio(params.get("file"), false);
+	}
 
 }
