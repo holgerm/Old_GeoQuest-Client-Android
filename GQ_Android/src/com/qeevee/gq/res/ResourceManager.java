e76278e9df874317b571a5581229166eeea9fc8a
Holger Muegge <muegge@acm.org>
2013-04-30 14:01:13 +0200

    added pos and neg standard sound paths

--- GQ_Android/src/com/qeevee/gq/res/ResourceManager.java
+++ GQ_Android/src/com/qeevee/gq/res/ResourceManager.java
@@ -6,19 +6,22 @@ import edu.bonn.mobilegaming.geoquest.Globals;
 
 public class ResourceManager {
 
-    public static String getResourcePath(String specifiedPath) {
+	public static String getResourcePath(String specifiedPath) {
 
-	if (specifiedPath.startsWith(Globals.RUNTIME_RESOURCE_PREFIX)) {
-	    String filepath = specifiedPath.substring(Globals.RUNTIME_RESOURCE_PREFIX
-		    .length());
-	    return Environment.getExternalStorageDirectory().getAbsolutePath()
-		    + "/" + filepath;
+		if (specifiedPath.startsWith(Globals.RUNTIME_RESOURCE_PREFIX)) {
+			String filepath = specifiedPath
+					.substring(Globals.RUNTIME_RESOURCE_PREFIX.length());
+			return Environment.getExternalStorageDirectory().getAbsolutePath()
+					+ "/" + filepath;
+		}
+
+		else {
+			return GeoQuestApp.getGameRessourceFile(specifiedPath)
+					.getAbsolutePath();
+		}
 	}
 
-	else {
-	    return GeoQuestApp.getGameRessourceFile(specifiedPath)
-		    .getAbsolutePath();
-	}
-    }
+	public static final String POSITIVE_SOUND = "audio/positive_sound.mp4";
+	public static final String NEGATIVE_SOUND = "audio/negative_sound.mp4";
 
 }
