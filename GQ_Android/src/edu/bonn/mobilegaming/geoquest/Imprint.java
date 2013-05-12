5db1e13eb19ac8a787b65b39d5523be2031ff6f5
Holger Muegge <muegge@acm.org>
2013-05-12 23:05:32 +0200

    first version modeled with most needed data.

--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/Imprint.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/Imprint.java
@@ -4,6 +4,19 @@ import org.dom4j.Element;
 
 public class Imprint {
 
+	String name;
+	String form; // TODO rename rechtsform engl.
+	String street;
+	int nr;
+	int plz; // TODO rename rechtsform engl.
+	String city;
+	String email;
+	String phone;
+	String fax;
+	String registerSitz; // TODO rename rechtsform engl.
+	String registerNr; // TODO rename rechtsform engl.
+	String ustIdNr; // TODO rename rechtsform engl.
+
 	public Imprint(Element imprintElement) {
 		// TODO Auto-generated constructor stub
 	}
