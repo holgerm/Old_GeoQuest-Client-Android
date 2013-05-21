18e6d52d1db836e9b2d17c5b826d035cc47ae1a5
Holger Muegge <muegge@acm.org>
2013-05-21 13:43:32 +0200

    format

--- GQ_Android/test/com/qeevee/gq/tests/gamedata/ImageResourceTests.java
+++ GQ_Android/test/com/qeevee/gq/tests/gamedata/ImageResourceTests.java
@@ -16,124 +16,116 @@ import com.qeevee.ui.BitmapUtil;
 @RunWith(GQTestRunner.class)
 public class ImageResourceTests {
 
-    Throwable thrown;
-    Bitmap searchedBitmap;
+	Throwable thrown;
+	Bitmap searchedBitmap;
 
-    @Before
-    public void init() {
-	thrown = null;
-	searchedBitmap = null;
-    }
-
-    // === TESTS FOLLOW =============================================
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void explicitlyReferingJPG() {
-	// GIVEN:
-	// nothing
-
-	// WHEN:
-	startGameForTest("ImageResourceTest");
-
-	// THEN:
-	shouldFindBitmap("drawable/jpgFile.jpg");
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void explicitlyReferingPNG() {
-	// GIVEN:
-	// nothing
-
-	// WHEN:
-	startGameForTest("ImageResourceTest");
-
-	// THEN:
-	shouldFindBitmap("drawable/pngFile.png");
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void implicitlyReferingPNG() {
-	// GIVEN:
-	// nothing
-
-	// WHEN:
-	startGameForTest("ImageResourceTest");
-
-	// THEN:
-	shouldFindBitmap("drawable/pngFile",
-			 "drawable/pngFile.png");
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void implicitlyReferingJPG() {
-	// GIVEN:
-	// nothing
-
-	// WHEN:
-	startGameForTest("ImageResourceTest");
-
-	// THEN:
-	shouldFindBitmap("drawable/jpgFile",
-			 "drawable/jpgFile.jpg");
-    }
-
-    @SuppressWarnings("unchecked")
-    @Test
-    public void ignoreTXTFile() {
-	// GIVEN:
-	// nothing
-
-	// WHEN:
-	startGameForTest("ImageResourceTest");
-
-	// THEN:
-	shouldIgnoreBitmap("drawable/txtFile");
-	shouldIgnoreBitmap("drawable/txtFile.txt");
-    }
-
-    // === HELPERS FOLLOW =============================================
-
-    private void shouldIgnoreBitmap(String pathToNONBitmapFile) {
-	try {
-	    searchedBitmap = BitmapUtil.loadBitmap(pathToNONBitmapFile,
-						   false);
-	} catch (Throwable t) {
-	    thrown = t;
+	@Before
+	public void init() {
+		thrown = null;
+		searchedBitmap = null;
 	}
-	assertEquals(IllegalArgumentException.class,
-		     thrown.getClass());
-	assertEquals(null, searchedBitmap);
-    }
 
-    private void shouldFindBitmap(String searchedBitmapPath,
-				  String pathToReferenceBitmap) {
-	Bitmap referenceBitmap = null;
-	try {
-	    searchedBitmap = BitmapUtil.loadBitmap(searchedBitmapPath,
-						   false);
-	    referenceBitmap = BitmapUtil.loadBitmap(pathToReferenceBitmap,
-						    false);
-	} catch (Throwable t) {
-	    thrown = t;
-	}
-	assertEquals(null,
-		     thrown);
-	assertEquals(referenceBitmap, searchedBitmap);
-    }
+	// === TESTS FOLLOW =============================================
 
-    private void shouldFindBitmap(String searchedBitmapPath) {
-	try {
-	    searchedBitmap = BitmapUtil.loadBitmap(searchedBitmapPath,
-						   false);
-	} catch (Throwable t) {
-	    thrown = t;
+	@SuppressWarnings("unchecked")
+	@Test
+	public void explicitlyReferingJPG() {
+		// GIVEN:
+		// nothing
+
+		// WHEN:
+		startGameForTest("ImageResourceTest");
+
+		// THEN:
+		shouldFindBitmap("drawable/jpgFile.jpg");
 	}
-	assertEquals(null,
-		     thrown);
-	assertTrue(searchedBitmap != null);
-    }
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void explicitlyReferingPNG() {
+		// GIVEN:
+		// nothing
+
+		// WHEN:
+		startGameForTest("ImageResourceTest");
+
+		// THEN:
+		shouldFindBitmap("drawable/pngFile.png");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void implicitlyReferingPNG() {
+		// GIVEN:
+		// nothing
+
+		// WHEN:
+		startGameForTest("ImageResourceTest");
+
+		// THEN:
+		shouldFindBitmap("drawable/pngFile", "drawable/pngFile.png");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void implicitlyReferingJPG() {
+		// GIVEN:
+		// nothing
+
+		// WHEN:
+		startGameForTest("ImageResourceTest");
+
+		// THEN:
+		shouldFindBitmap("drawable/jpgFile", "drawable/jpgFile.jpg");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void ignoreTXTFile() {
+		// GIVEN:
+		// nothing
+
+		// WHEN:
+		startGameForTest("ImageResourceTest");
+
+		// THEN:
+		shouldIgnoreBitmap("drawable/txtFile");
+		shouldIgnoreBitmap("drawable/txtFile.txt");
+	}
+
+	// === HELPERS FOLLOW =============================================
+
+	private void shouldIgnoreBitmap(String pathToNONBitmapFile) {
+		try {
+			searchedBitmap = BitmapUtil.loadBitmap(pathToNONBitmapFile, false);
+		} catch (Throwable t) {
+			thrown = t;
+		}
+		assertEquals(IllegalArgumentException.class, thrown.getClass());
+		assertEquals(null, searchedBitmap);
+	}
+
+	private void shouldFindBitmap(String searchedBitmapPath,
+			String pathToReferenceBitmap) {
+		Bitmap referenceBitmap = null;
+		try {
+			searchedBitmap = BitmapUtil.loadBitmap(searchedBitmapPath, false);
+			referenceBitmap = BitmapUtil.loadBitmap(pathToReferenceBitmap,
+					false);
+		} catch (Throwable t) {
+			thrown = t;
+		}
+		assertEquals(null, thrown);
+		assertEquals(referenceBitmap, searchedBitmap);
+	}
+
+	private void shouldFindBitmap(String searchedBitmapPath) {
+		try {
+			searchedBitmap = BitmapUtil.loadBitmap(searchedBitmapPath, false);
+		} catch (Throwable t) {
+			thrown = t;
+		}
+		assertEquals(null, thrown);
+		assertTrue(searchedBitmap != null);
+	}
 }
