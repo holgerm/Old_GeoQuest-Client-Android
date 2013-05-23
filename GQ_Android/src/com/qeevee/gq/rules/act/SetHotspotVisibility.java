8e5a5147d851237a65bfaa79ac21d074796c62b4
Holger Muegge <muegge@acm.org>
2013-05-23 20:44:24 +0200

    format

--- GQ_Android/src/com/qeevee/gq/rules/act/SetHotspotVisibility.java
+++ GQ_Android/src/com/qeevee/gq/rules/act/SetHotspotVisibility.java
@@ -5,24 +5,24 @@ import edu.bonn.mobilegaming.geoquest.HotspotOld;
 
 public class SetHotspotVisibility extends Action {
 
-    @Override
-    protected boolean checkInitialization() {
-	return params.containsKey("id") && params.containsKey("visible");
-    }
-
-    @Override
-    public void execute() {
-	HotspotOld hotspot = HotspotOld.getExisting(params.get("id"));
-	if (hotspot == null)
-	    return;
-	if (params.get("visible").equals("true")) {
-	    hotspot.setVisible(true);
-	}
-	if (params.get("visible").equals("false")) {
-	    hotspot.setVisible(false);
+	@Override
+	protected boolean checkInitialization() {
+		return params.containsKey("id") && params.containsKey("visible");
 	}
 
-	GeoQuestApp.getInstance().refreshMapDisplay();
-    }
+	@Override
+	public void execute() {
+		HotspotOld hotspot = HotspotOld.getExisting(params.get("id"));
+		if (hotspot == null)
+			return;
+		if (params.get("visible").equals("true")) {
+			hotspot.setVisible(true);
+		}
+		if (params.get("visible").equals("false")) {
+			hotspot.setVisible(false);
+		}
+
+		GeoQuestApp.getInstance().refreshMapDisplay();
+	}
 
 }
