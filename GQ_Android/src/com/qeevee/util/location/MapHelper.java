f562c6298086fdcb0f20ff0bbc004e4edd6d8a9a
Holger Muegge <muegge@acm.org>
2013-05-24 09:21:12 +0200

    format

--- GQ_Android/src/com/qeevee/util/location/MapHelper.java
+++ GQ_Android/src/com/qeevee/util/location/MapHelper.java
@@ -13,73 +13,73 @@ import edu.bonn.mobilegaming.geoquest.mission.MapNavigation;
 
 public class MapHelper {
 
-    private IMapController mapController;
-    private GeoQuestLocationListener locationListener;
-    private MapNavigation mapMission;
+	private IMapController mapController;
+	private GeoQuestLocationListener locationListener;
+	private MapNavigation mapMission;
 
-    /**
-     * Constructor for Google Map based Navigation Mission.
-     * 
-     * @param mapMission
-     * @param mapCtrl
-     */
-    public MapHelper(final MapNavigation mapMission) {
-	this.mapMission = mapMission;
-	mapController = mapMission.getMapController();
+	/**
+	 * Constructor for Google Map based Navigation Mission.
+	 * 
+	 * @param mapMission
+	 * @param mapCtrl
+	 */
+	public MapHelper(final MapNavigation mapMission) {
+		this.mapMission = mapMission;
+		mapController = mapMission.getMapController();
 
-	// register location changed listener:
-	locationListener = new GeoQuestLocationListener(mapMission) {
-	    public void onRelevantLocationChanged(Location location) {
-		super.onRelevantLocationChanged(location);
-		GeoPoint point = location2GP(location);
-		mapController.animateTo(point);
+		// register location changed listener:
+		locationListener = new GeoQuestLocationListener(mapMission) {
+			public void onRelevantLocationChanged(Location location) {
+				super.onRelevantLocationChanged(location);
+				GeoPoint point = location2GP(location);
+				mapController.animateTo(point);
 
-		// calculate distance to hotspots
-		for (Iterator<HotspotOld> i = mapMission.getHotspots()
-			.listIterator(); i.hasNext();) {
-		    HotspotOld hotspot = i.next(); // TODO: throws a
-						   // ConcurrentModificationException
-						   // sometimes (hm)
-		    hotspot.inRange(location);
-		}
-	    }
-	};
-    }
-
-    public void centerMap() {
-	Location lastLoc = locationListener.getLastLocation();
-	if (lastLoc != null)
-	    mapController.animateTo(location2GP(locationListener
-		    .getLastLocation()));
-    }
-
-    public void setCenter() {
-	Location lastLoc = locationListener.getLastLocation();
-	if (lastLoc != null)
-	    mapController.setCenter(location2GP(locationListener
-		    .getLastLocation()));
-	else {
-	    List<HotspotOld> hotspots = this.mapMission.getHotspots();
-	    if (hotspots.size() > 0) {
-		com.google.android.maps.GeoPoint firstAGP = hotspots.get(0)
-			.getPosition();
-		GeoPoint centerGP = new GeoPoint(firstAGP.getLatitudeE6(),
-			firstAGP.getLongitudeE6());
-		mapController.setCenter(centerGP);
-	    }
+				// calculate distance to hotspots
+				for (Iterator<HotspotOld> i = mapMission.getHotspots()
+						.listIterator(); i.hasNext();) {
+					HotspotOld hotspot = i.next(); // TODO: throws a
+					// ConcurrentModificationException
+					// sometimes (hm)
+					hotspot.inRange(location);
+				}
+			}
+		};
 	}
-    }
 
-    private GeoPoint location2GP(Location location) {
-	if (location == null)
-	    return null;
-	GeoPoint point = new GeoPoint((int) (location.getLatitude() * 1E6),
-		(int) (location.getLongitude() * 1E6));
-	return point;
-    }
+	public void centerMap() {
+		Location lastLoc = locationListener.getLastLocation();
+		if (lastLoc != null)
+			mapController.animateTo(location2GP(locationListener
+					.getLastLocation()));
+	}
 
-    public GeoQuestLocationListener getLocationListener() {
-	return locationListener;
-    }
+	public void setCenter() {
+		Location lastLoc = locationListener.getLastLocation();
+		if (lastLoc != null)
+			mapController.setCenter(location2GP(locationListener
+					.getLastLocation()));
+		else {
+			List<HotspotOld> hotspots = this.mapMission.getHotspots();
+			if (hotspots.size() > 0) {
+				com.google.android.maps.GeoPoint firstAGP = hotspots.get(0)
+						.getPosition();
+				GeoPoint centerGP = new GeoPoint(firstAGP.getLatitudeE6(),
+						firstAGP.getLongitudeE6());
+				mapController.setCenter(centerGP);
+			}
+		}
+	}
+
+	private GeoPoint location2GP(Location location) {
+		if (location == null)
+			return null;
+		GeoPoint point = new GeoPoint((int) (location.getLatitude() * 1E6),
+				(int) (location.getLongitude() * 1E6));
+		return point;
+	}
+
+	public GeoQuestLocationListener getLocationListener() {
+		return locationListener;
+	}
 
 }
--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/MapNavigation.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/MapNavigation.java
@@ -27,141 +27,136 @@ import edu.bonn.mobilegaming.geoquest.R;
 import edu.bonn.mobilegaming.geoquest.ui.abstrakt.MissionOrToolUI;
 
 public abstract class MapNavigation extends GeoQuestMapActivity implements
-	HotspotListener {
+		HotspotListener {
 
-    public MissionOrToolUI getUI() {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    public void onBlockingStateUpdated(boolean isBlocking) {
-	// TODO move into UI
-
-    }
-
-    @Override
-    protected boolean isRouteDisplayed() {
-	return false;
-    }
-
-    public abstract IMapView getMapView();
-
-    public abstract IMapController getMapController();
-
-    /**
-     * list of hotspots, inited in readxml. main thread may not access this
-     * until readxml_completed is true
-     * */
-    private List<HotspotOld> hotspots = new ArrayList<HotspotOld>();
-    protected MapHelper mapHelper;
-    protected LocationManager myLocationManager;
-    protected Handler handler = new Handler();
-    protected LocationSource locationSource;
-    protected Mission mission;
-
-    public void onEnterRange(HotspotOld h) {
-	// TODO obsolete?
-    }
-
-    public void onLeaveRange(HotspotOld h) {
-	// TODO obsolete?
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-	Log.d(this.getClass().getName(),
-	      "creating activity");
-	super.onCreate(savedInstanceState);
-	initMission();
-    }
-
-    private void initMission() {
-	Bundle extras = getIntent().getExtras();
-	String id = extras.getString("missionID");
-	mission = Mission.get(id);
-	mission.setStatus(Globals.STATUS_RUNNING);
-    }
-
-    public List<HotspotOld> getHotspots() {
-	return hotspots;
-    }
-
-    protected void initZoom() {
-	getMapController().setZoom(18);
-	String zoomLevel = mission.xmlMissionNode.attributeValue("zoomlevel");
-	if (zoomLevel != null) {
-	    int zoomLevelInt = Integer.parseInt(zoomLevel);
-	    if (zoomLevelInt > 0
-		    && zoomLevelInt < 24)
-		getMapController().setZoom(zoomLevelInt);
+	public MissionOrToolUI getUI() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	// Setup Zoom Controls:
-	Button zoomIn = (Button) findViewById(R.id.zoom_in);
-	zoomIn.setOnClickListener(new OnClickListener() {
+	public void onBlockingStateUpdated(boolean isBlocking) {
+		// TODO move into UI
 
-	    public void onClick(View v) {
-		getMapController().zoomIn();
-	    }
-	});
-
-	Button zoomOut = (Button) findViewById(R.id.zoom_out);
-	zoomOut.setOnClickListener(new OnClickListener() {
-
-	    public void onClick(View v) {
-		getMapController().zoomOut();
-	    }
-	});
-
-    }
-
-    protected void initGPSMock() {
-	try {
-	    long timeStepMockMode = Long
-		    .parseLong(getText(R.string.map_mockGPSTimeInterval)
-			    .toString());
-	    locationSource = new LocationSource(getApplicationContext(),
-		    mapHelper.getLocationListener(), handler, timeStepMockMode);
-	    locationSource.setMode(LocationSource.REAL_MODE);
-	} catch (Exception e) {
-	    e.printStackTrace();
 	}
-    }
 
-    /**
-     * Finishes the mission activity and sets the result code
-     * 
-     * @param status
-     *            Mission.STATUS_SUCCESS or FAIL or NEW
-     */
-    public void finish(Double status) {
-	mission.setStatus(status);
-	mission.applyOnEndRules();
-	finish();
-    }
-
-    /**
-     * Back button Handler quits the Mission, when back button is hit.
-     */
-    @Override
-    public boolean onKeyDown(final int keyCode,
-			     KeyEvent event) {
-	switch (keyCode) {
-
-	case KeyEvent.KEYCODE_BACK: // Back => Cancel
-	    if (mission.cancelStatus == 0) {
-		Log.d(this.getClass().getName(),
-		      "Back Button was pressed, but mission may not be cancelled.");
-		return true;
-	    } else {
-		finish(mission.cancelStatus);
-		return true;
-	    }
-	case KeyEvent.KEYCODE_SEARCH:
-	    // ignore search button
-	    break;
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
 	}
-	return super.onKeyDown(keyCode,
-			       event);
-    }
+
+	public abstract IMapView getMapView();
+
+	public abstract IMapController getMapController();
+
+	/**
+	 * list of hotspots, inited in readxml. main thread may not access this
+	 * until readxml_completed is true
+	 * */
+	private List<HotspotOld> hotspots = new ArrayList<HotspotOld>();
+	protected MapHelper mapHelper;
+	protected LocationManager myLocationManager;
+	protected Handler handler = new Handler();
+	protected LocationSource locationSource;
+	protected Mission mission;
+
+	public void onEnterRange(HotspotOld h) {
+		// TODO obsolete?
+	}
+
+	public void onLeaveRange(HotspotOld h) {
+		// TODO obsolete?
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(this.getClass().getName(), "creating activity");
+		super.onCreate(savedInstanceState);
+		initMission();
+	}
+
+	private void initMission() {
+		Bundle extras = getIntent().getExtras();
+		String id = extras.getString("missionID");
+		mission = Mission.get(id);
+		mission.setStatus(Globals.STATUS_RUNNING);
+	}
+
+	public List<HotspotOld> getHotspots() {
+		return hotspots;
+	}
+
+	protected void initZoom() {
+		getMapController().setZoom(18);
+		String zoomLevel = mission.xmlMissionNode.attributeValue("zoomlevel");
+		if (zoomLevel != null) {
+			int zoomLevelInt = Integer.parseInt(zoomLevel);
+			if (zoomLevelInt > 0 && zoomLevelInt < 24)
+				getMapController().setZoom(zoomLevelInt);
+		}
+
+		// Setup Zoom Controls:
+		Button zoomIn = (Button) findViewById(R.id.zoom_in);
+		zoomIn.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				getMapController().zoomIn();
+			}
+		});
+
+		Button zoomOut = (Button) findViewById(R.id.zoom_out);
+		zoomOut.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				getMapController().zoomOut();
+			}
+		});
+
+	}
+
+	protected void initGPSMock() {
+		try {
+			long timeStepMockMode = Long.parseLong(getText(
+					R.string.map_mockGPSTimeInterval).toString());
+			locationSource = new LocationSource(getApplicationContext(),
+					mapHelper.getLocationListener(), handler, timeStepMockMode);
+			locationSource.setMode(LocationSource.REAL_MODE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Finishes the mission activity and sets the result code
+	 * 
+	 * @param status
+	 *            Mission.STATUS_SUCCESS or FAIL or NEW
+	 */
+	public void finish(Double status) {
+		mission.setStatus(status);
+		mission.applyOnEndRules();
+		finish();
+	}
+
+	/**
+	 * Back button Handler quits the Mission, when back button is hit.
+	 */
+	@Override
+	public boolean onKeyDown(final int keyCode, KeyEvent event) {
+		switch (keyCode) {
+
+		case KeyEvent.KEYCODE_BACK: // Back => Cancel
+			if (mission.cancelStatus == 0) {
+				Log.d(this.getClass().getName(),
+						"Back Button was pressed, but mission may not be cancelled.");
+				return true;
+			} else {
+				finish(mission.cancelStatus);
+				return true;
+			}
+		case KeyEvent.KEYCODE_SEARCH:
+			// ignore search button
+			break;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
 }
