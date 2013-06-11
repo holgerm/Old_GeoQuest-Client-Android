38f1f9e7b384b7ea80b3bf2755011757226a6fc0
Holger Muegge <muegge@acm.org>
2013-06-11 09:22:20 +0200

    format

--- GQ_Android/test/com/qeevee/gq/tests/mission/ui/standard/NPCTalkUIDefaultTests.java
+++ GQ_Android/test/com/qeevee/gq/tests/mission/ui/standard/NPCTalkUIDefaultTests.java
@@ -30,171 +30,161 @@ import edu.bonn.mobilegaming.geoquest.ui.standard.NPCTalkUIDefault.WordTicker;
 
 @RunWith(GQTestRunner.class)
 public class NPCTalkUIDefaultTests {
-    NPCTalkUIDefault ui;
-    ZoomImageView imageView;
-    TextView textView;
-    Button proceedBT;
-    private Start start;
-    private NPCTalk npcTalk;
-    private WordTicker ticker;
+	NPCTalkUIDefault ui;
+	ZoomImageView imageView;
+	TextView textView;
+	Button proceedBT;
+	private Start start;
+	private NPCTalk npcTalk;
+	private WordTicker ticker;
 
-    public void initUIFields() {
-	ui = (NPCTalkUIDefault) getFieldValue(npcTalk,
-					      "ui");
-	imageView = (ZoomImageView) getFieldValue(ui,
-						  "charImage");
-	textView = (TextView) getFieldValue(ui,
-					    "dialogText");
-	proceedBT = (Button) getFieldValue(ui,
-					   "button");
-	ticker = (WordTicker) getFieldValue(ui,
-					    "ticker");
-    }
+	public void initUIFields() {
+		ui = (NPCTalkUIDefault) getFieldValue(npcTalk, "ui");
+		imageView = (ZoomImageView) getFieldValue(ui, "charImage");
+		textView = (TextView) getFieldValue(ui, "dialogText");
+		proceedBT = (Button) getFieldValue(ui, "button");
+		ticker = (WordTicker) getFieldValue(ui, "ticker");
+	}
 
-    @After
-    public void cleanUp() {
-	// get rid of all variables that have been set, e.g. for checking
-	// actions.
-	Variables.clean();
-	History.getInstance().clear();
-    }
+	@After
+	public void cleanUp() {
+		// get rid of all variables that have been set, e.g. for checking
+		// actions.
+		Variables.clean();
+		History.getInstance().clear();
+	}
 
-    // === TESTS FOLLOW =============================================
+	// === TESTS FOLLOW =============================================
 
-    @SuppressWarnings("unchecked")
-    @Test
-    public void startFirstMission_DoNotShowAllWordsYet() {
-	// GIVEN:
-	start = startGameForTest("npctalk/NPCTalkStandard",
-				 DefaultUIFactory.class);
-	npcTalk = (NPCTalk) prepareMission("NPCTalk",
-					   "m1",
-					   start);
+	@SuppressWarnings("unchecked")
+	@Test
+	public void startFirstMission_DoNotShowAllWordsYet() {
+		// GIVEN:
+		start = startGameForTest("npctalk/NPCTalkStandard",
+				DefaultUIFactory.class);
+		npcTalk = (NPCTalk) prepareMission("NPCTalk", "m1", start);
 
-	// WHEN:
-	startMission(npcTalk);
+		// WHEN:
+		startMission(npcTalk);
 
-	// THEN:
-	buttonShouldBeDisabled();
-	buttonShouldShowNextDialogItemText();
-    }
+		// THEN:
+		buttonShouldBeDisabled();
+		buttonShouldShowNextDialogItemText();
+	}
 
-    @SuppressWarnings("unchecked")
-    @Test
-    public void wordTickerDisplaysTextWordByWord() {
-	// GIVEN:
-	start = startGameForTest("npctalk/WordCountTest",
-				 DefaultUIFactory.class);
-	npcTalk = (NPCTalk) prepareMission("NPCTalk",
-					   "3210WordsInDialogItems",
-					   start);
-	startMission(npcTalk);
+	@SuppressWarnings("unchecked")
+	@Test
+	public void wordTickerDisplaysTextWordByWord() {
+		// GIVEN:
+		start = startGameForTest("npctalk/WordCountTest",
+				DefaultUIFactory.class);
+		npcTalk = (NPCTalk) prepareMission("NPCTalk", "3210WordsInDialogItems",
+				start);
+		startMission(npcTalk);
 
-	// WHEN:
-	ticker.onTick(0);
-	// THEN:
-	npcTextShouldShowAtEnd("One ");
+		// WHEN:
+		ticker.onTick(0);
+		// THEN:
+		npcTextShouldShowAtEnd("One ");
 
-	// WHEN:
-	ticker.onTick(0);
-	// THEN:
-	npcTextShouldShowAtEnd("two ");
+		// WHEN:
+		ticker.onTick(0);
+		// THEN:
+		npcTextShouldShowAtEnd("two ");
 
-	// WHEN:
-	ticker.onTick(0);
-	// THEN:
-	npcTextShouldShowAtEnd("three\n");
-    }
+		// WHEN:
+		ticker.onTick(0);
+		// THEN:
+		npcTextShouldShowAtEnd("three\n");
+	}
 
-    @SuppressWarnings("unchecked")
-    @Test
-    public void buttonEnabledOnlyWhenWordTickerFinished() {
-	// GIVEN:
-	start = startGameForTest("npctalk/WordCountTest",
-				 DefaultUIFactory.class);
-	npcTalk = (NPCTalk) prepareMission("NPCTalk",
-					   "3210WordsInDialogItems",
-					   start);
-	startMission(npcTalk);
-	ticker.onTick(0);
-	ticker.onTick(0);
-	ticker.onTick(0);
+	@SuppressWarnings("unchecked")
+	@Test
+	public void buttonEnabledOnlyWhenWordTickerFinished() {
+		// GIVEN:
+		start = startGameForTest("npctalk/WordCountTest",
+				DefaultUIFactory.class);
+		npcTalk = (NPCTalk) prepareMission("NPCTalk", "3210WordsInDialogItems",
+				start);
+		startMission(npcTalk);
+		ticker.onTick(0);
+		ticker.onTick(0);
+		ticker.onTick(0);
 
-	// THEN:
-	buttonShouldBeDisabled();
+		// THEN:
+		buttonShouldBeDisabled();
 
-	// WHEN:
-	ticker.onFinish();
-	// THEN:
-	npcTextShouldShowAtEnd("One two three\n");
-	buttonShouldBeEnabled();
-	buttonShouldShowNextDialogItemText();
-    }
+		// WHEN:
+		ticker.onFinish();
+		// THEN:
+		npcTextShouldShowAtEnd("One two three\n");
+		buttonShouldBeEnabled();
+		buttonShouldShowNextDialogItemText();
+	}
 
-    @SuppressWarnings("unchecked")
-    @Test
-    public void goToThroughDialogItems() {
-	// GIVEN:
-	start = startGameForTest("npctalk/WordCountTest",
-				 DefaultUIFactory.class);
-	npcTalk = (NPCTalk) prepareMission("NPCTalk",
-					   "3210WordsInDialogItems",
-					   start);
-	startMission(npcTalk);
-	ticker.onFinish();
+	@SuppressWarnings("unchecked")
+	@Test
+	public void goToThroughDialogItems() {
+		// GIVEN:
+		start = startGameForTest("npctalk/WordCountTest",
+				DefaultUIFactory.class);
+		npcTalk = (NPCTalk) prepareMission("NPCTalk", "3210WordsInDialogItems",
+				start);
+		startMission(npcTalk);
+		ticker.onFinish();
 
-	// WHEN:
-	proceedBT.performClick();
+		// WHEN:
+		proceedBT.performClick();
 
-	// THEN:
-	buttonShouldBeDisabled();
+		// THEN:
+		buttonShouldBeDisabled();
 
-	// WHEN:
-	ticker.onFinish();
+		// WHEN:
+		ticker.onFinish();
 
-	// THEN:
-	npcTextShouldShowAtEnd("Just two\n");
-	buttonShouldBeEnabled();
+		// THEN:
+		npcTextShouldShowAtEnd("Just two\n");
+		buttonShouldBeEnabled();
 
-	// WHEN:
-	proceedBT.performClick();
-	ticker.onFinish();
+		// WHEN:
+		proceedBT.performClick();
+		ticker.onFinish();
 
-	// THEN:
-	npcTextShouldShowAtEnd("Emptyfollows\n");
-	buttonShouldBeEnabled();
+		// THEN:
+		npcTextShouldShowAtEnd("Emptyfollows\n");
+		buttonShouldBeEnabled();
 
-	// WHEN:
-	proceedBT.performClick();
-	ticker.onFinish();
+		// WHEN:
+		proceedBT.performClick();
+		ticker.onFinish();
 
-	// THEN:
-	npcTextShouldShowAtEnd("Emptyfollows\n\n");
-	buttonShouldBeEnabled();
-    }
+		// THEN:
+		npcTextShouldShowAtEnd("Emptyfollows\n\n");
+		buttonShouldBeEnabled();
+	}
 
-    // === HELPER METHODS FOLLOW =============================================
+	// === HELPER METHODS FOLLOW =============================================
 
-    private void startMission(MissionActivity mission) {
-	mission.onCreate(null);
-	initUIFields();
-    }
+	private void startMission(MissionActivity mission) {
+		mission.onCreate(null);
+		initUIFields();
+	}
 
-    private void buttonShouldBeEnabled() {
-	assertTrue(proceedBT.isEnabled());
-    }
+	private void buttonShouldBeEnabled() {
+		assertTrue(proceedBT.isEnabled());
+	}
 
-    private void buttonShouldBeDisabled() {
-	assertFalse(proceedBT.isEnabled());
-    }
+	private void buttonShouldBeDisabled() {
+		assertFalse(proceedBT.isEnabled());
+	}
 
-    private void buttonShouldShowNextDialogItemText() {
-	assertEquals(TestUtils.getResString(R.string.button_text_next),
-		     proceedBT.getText());
-    }
+	private void buttonShouldShowNextDialogItemText() {
+		assertEquals(TestUtils.getResString(R.string.button_text_next),
+				proceedBT.getText());
+	}
 
-    private void npcTextShouldShowAtEnd(String expectedStringAtEnd) {
-	assertTrue(textView.getText().toString().endsWith(expectedStringAtEnd));
-    }
+	private void npcTextShouldShowAtEnd(String expectedStringAtEnd) {
+		assertTrue(textView.getText().toString().endsWith(expectedStringAtEnd));
+	}
 
 }
