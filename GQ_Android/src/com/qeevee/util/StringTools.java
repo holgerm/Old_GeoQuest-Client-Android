fd39c6d9cb890be94a79652518cc4e948cc6e937
Holger Muegge <muegge@acm.org>
2013-07-09 09:49:51 +0200

    bugs fixed with variable replacement regarding system variables

--- GQ_Android/src/com/qeevee/util/StringTools.java
+++ GQ_Android/src/com/qeevee/util/StringTools.java
@@ -4,9 +4,13 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import android.util.Log;
 import edu.bonn.mobilegaming.geoquest.Variables;
 
 public class StringTools {
+
+	private static final Pattern varReplacePattern = Pattern
+			.compile("@(\\$_)?[\\w.]+@");
 
 	public static String caseUp(String original) {
 		return original.substring(0, 1).toUpperCase(Locale.ENGLISH)
@@ -19,14 +23,16 @@ public class StringTools {
 
 	public static String replaceVariables(String inputString) {
 		String result = new String(inputString);
-		Pattern p = Pattern.compile("@\\$?\\w+@");
-		Matcher m = p.matcher(inputString);
+		Matcher m = varReplacePattern.matcher(inputString);
 		while (m.find()) {
 			String varComplete = m.group();
 			String varName = varComplete.substring(1, varComplete.length() - 1);
 			if (Variables.isDefined(varName)) {
-				result = result.replaceAll(varComplete,
-						Variables.getValue(varName).toString());
+				result = result.replace(varComplete, Variables
+						.getValue(varName).toString());
+			} else {
+				Log.w(StringTools.class.getCanonicalName(),
+						"Undefined variable found: " + varName);
 			}
 		}
 
--- GQ_Android/test/com/qeevee/gq/tests/xml/TestVariableReplacementInText.java
+++ GQ_Android/test/com/qeevee/gq/tests/xml/TestVariableReplacementInText.java
@@ -27,6 +27,7 @@ public class TestVariableReplacementInText {
 		Variables.setValue("myVar", "Hello");
 		Variables.setValue("yourVar", "are");
 		Variables.setValue("herVar", "?");
+
 		// WHEN:
 		String input = "@myVar@ how @yourVar@ you@herVar@";
 
@@ -34,7 +35,45 @@ public class TestVariableReplacementInText {
 		assertEquals("Hello how are you?", StringTools.replaceVariables(input));
 	}
 
+	@Test
+	public void replaceMultipleOccurences() {
+		// GIVEN:
+		Variables.setValue("myVar", "hello");
 
+		// WHEN:
+		String input = "I say @myVar@, @myVar@ and @myVar@ again.";
+
+		// THEN:
+		assertEquals("I say hello, hello and hello again.",
+				StringTools.replaceVariables(input));
+	}
+
+	@Test
+	public void useGQSystemVariables() {
+		// GIVEN:
+		Variables.setValue("$_gqVar.state", "the value");
+
+		// WHEN:
+		String input = "Pretext @$_gqVar.state@ posttext";
+
+		// THEN:
+		assertEquals("Pretext the value posttext",
+				StringTools.replaceVariables(input));
+	}
+
+	@Test
+	public void logUsageOfUndefinedVariables() {
+		// GIVEN:
+		Variables.setValue("greeting", "Good day");
+
+		// WHEN:
+		String input = "I say @greeting@ @undefinedVar@!";
+
+		// THEN:
+		assertEquals("I say Good day @undefinedVar@!",
+				StringTools.replaceVariables(input));
+		// TODO check that access has been logged
+	}
 
 	// === HELPER METHODS FOLLOW =============================================
 
