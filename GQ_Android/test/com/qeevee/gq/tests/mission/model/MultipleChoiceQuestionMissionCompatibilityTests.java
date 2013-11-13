00a7a6ce697f6a6139bbc3e76fb72910dbd6c239
Holger Muegge <muegge@acm.org>
2013-11-13 13:26:17 +0100

    question tag is now optional. Additional test exists and runs.

---
+++ GQ_Android/test/com/qeevee/gq/tests/mission/model/MultipleChoiceQuestionMissionCompatibilityTests.java
@@ -0,0 +1,23 @@
+package com.qeevee.gq.tests.mission.model;
+
+import org.junit.runner.RunWith;
+
+import com.qeevee.gq.tests.robolectric.GQTestRunner;
+
+/**
+ * In contrast to the FunctionTest this one loads a game.xml which still uses
+ * the question xml tag inside the mission as parent of questiontext and
+ * answers.
+ * 
+ * The rest is completely the same.
+ * 
+ * @author muegge
+ * 
+ */
+@RunWith(GQTestRunner.class)
+public class MultipleChoiceQuestionMissionCompatibilityTests extends
+		MultipleChoiceQuestionMissionTests {
+
+	private static final String GAME_NAME = "MultipleChoiceQuestion/FunctionCompatibilityTest";
+
+}
