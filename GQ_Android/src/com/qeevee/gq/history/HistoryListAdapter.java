306585eb2b8fa58a8b650fe07326f123cdbf8c25
Holger Muegge <muegge@acm.org>
2014-02-06 00:15:52 +0100

    bug fixed. history npe in non-npctalk missions

--- GQ_Android/src/com/qeevee/gq/history/HistoryListAdapter.java
+++ GQ_Android/src/com/qeevee/gq/history/HistoryListAdapter.java
@@ -7,28 +7,26 @@ import android.widget.ListAdapter;
 
 public class HistoryListAdapter extends BaseAdapter implements ListAdapter {
 
-    private History history;
+	private History history;
 
-    public HistoryListAdapter() {
-	history = History.getInstance();
-    }
+	public HistoryListAdapter() {
+		history = History.getInstance();
+	}
 
-    public int getCount() {
-	return history.numberOfItems();
-    }
+	public int getCount() {
+		return history.numberOfItems();
+	}
 
-    public Object getItem(int position) {
-	return history.getItem(position);
-    }
+	public Object getItem(int position) {
+		return history.getItem(position);
+	}
 
-    public long getItemId(int position) {
-	return history.getItemId(position);
-    }
+	public long getItemId(int position) {
+		return history.getItemId(position);
+	}
 
-    public View getView(int position,
-			View convertView,
-			ViewGroup parent) {
-	return history.getItem(position).getView(convertView);
-    }
+	public View getView(int position, View convertView, ViewGroup parent) {
+		return history.getItem(position).getView(convertView);
+	}
 
 }
