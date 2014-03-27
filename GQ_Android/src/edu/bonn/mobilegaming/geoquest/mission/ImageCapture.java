03d076cca4af9ed504bfc96f7ddc42f9df337f1e
Holger Muegge <muegge@acm.org>
2014-03-28 00:03:42 +0100

    fixed image capture store file bug

--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/ImageCapture.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/ImageCapture.java
@@ -19,9 +19,9 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,6 +29,7 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.qeevee.gq.res.ResourceManager;
 import com.qeevee.gq.xml.XMLUtilities;
 import com.qeevee.ui.BitmapUtil;
 import com.qeevee.ui.ZoomImageView;
@@ -62,6 +63,8 @@ public class ImageCapture extends MissionActivity implements OnClickListener {
 
 	private String mFileName;
 
+	private String mCurrentPhotoPath;
+
 	// private File localImageFile = null;
 
 	@Override
@@ -84,10 +87,8 @@ public class ImageCapture extends MissionActivity implements OnClickListener {
 		uploadURL = getMissionAttribute("uploadURL",
 				XMLUtilities.OPTIONAL_ATTRIBUTE);
 
-		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath()
-				+ "/"
-				+ getMissionAttribute("file",
-						R.string.imageCapture_file_default);
+		mFileName = getMissionAttribute("file",
+				R.string.imageCapture_file_default).toString();
 
 		// initial image:
 		imageView = (ZoomImageView) findViewById(R.id.imageCaptureImageView);
@@ -114,7 +115,7 @@ public class ImageCapture extends MissionActivity implements OnClickListener {
 		super.onResume();
 	}
 
-	private static final int TAKE_PICTURE = 1;
+	private static final int TAKE_PICTURE = 123;
 	private static final int AFTER_UPLOAD = 2;
 
 	@Override
@@ -128,17 +129,39 @@ public class ImageCapture extends MissionActivity implements OnClickListener {
 	public void onClick(View v) {
 		switch (endButtonMode) {
 		case TAKE_PICTURE:
-			File file = new File(mFileName);
-			Uri outputFileUri = Uri.fromFile(file);
-			Intent intentTakePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-			intentTakePhoto.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-			startActivityForResult(intentTakePhoto, TAKE_PICTURE);
+			// takePicture();
+			takePicture();
 			break;
 		case AFTER_UPLOAD:
 			finish(Globals.STATUS_SUCCEEDED);
 			break;
 		}
 		return;
+	}
+
+	private void takePicture() {
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		File f = null;
+
+		try {
+			f = setUpPhotoFile();
+			mCurrentPhotoPath = f.getAbsolutePath();
+			takePictureIntent
+					.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+		} catch (IOException e) {
+			e.printStackTrace();
+			f = null;
+			mCurrentPhotoPath = null;
+		}
+		startActivityForResult(takePictureIntent, TAKE_PICTURE);
+	}
+
+	private File setUpPhotoFile() throws IOException {
+		File f = ResourceManager.getImageFile(mFileName);
+		mCurrentPhotoPath = f.getAbsolutePath();
+
+		return f;
 	}
 
 	public void uploadBitmap(Bitmap bitmap) {
@@ -190,17 +213,58 @@ public class ImageCapture extends MissionActivity implements OnClickListener {
 
 	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 		if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK) {
-			Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
-
-			// redefine UI:
-			imageView.setImageBitmap(bitmap);
-			if (uploadURL != null)
-				uploadBitmap(bitmap);
+			if (mCurrentPhotoPath != null) {
+				setPic();
+				galleryAddPic();
+				mCurrentPhotoPath = null;
+			}
 			setMode(AFTER_UPLOAD);
-
 		} else {
 			GeoQuestApp.showMessage("Picture not taken.");
 		}
+	}
+
+	private void galleryAddPic() {
+		Intent mediaScanIntent = new Intent(
+				"android.intent.action.MEDIA_SCANNER_SCAN_FILE");
+		File f = new File(mCurrentPhotoPath);
+		Uri contentUri = Uri.fromFile(f);
+		mediaScanIntent.setData(contentUri);
+		this.sendBroadcast(mediaScanIntent);
+	}
+
+	private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+		int targetW = imageView.getWidth();
+		int targetH = imageView.getHeight();
+
+		/* Get the size of the image */
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+		int scaleFactor = 1;
+		if ((targetW > 0) || (targetH > 0)) {
+			scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+		}
+
+		/* Set bitmap options to scale the image decode target */
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor;
+		bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap */
+		Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+		/* Associate the Bitmap to the ImageView */
+		imageView.setImageBitmap(bitmap);
 	}
 
 	private void setMode(final int mode) {
@@ -208,7 +272,7 @@ public class ImageCapture extends MissionActivity implements OnClickListener {
 		switch (mode) {
 		case TAKE_PICTURE:
 			okButton.setText(getMissionAttribute("buttontext",
-					R.string.qrtagreading_startscanbutton_default));
+					R.string.imageCapture_button_takePicture));
 			break;
 		case AFTER_UPLOAD:
 			okButton.setText(getText(R.string.button_text_proceed));
