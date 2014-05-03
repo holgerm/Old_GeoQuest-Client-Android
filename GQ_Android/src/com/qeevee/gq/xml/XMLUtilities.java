a8214b6960c7b9f879ff3efa870717b5cef8d8dd
Holger Muegge <muegge@acm.org>
2014-05-03 09:51:33 +0200

    added integer typed xml attributes to convenient xml access with defaults etc.

--- GQ_Android/src/com/qeevee/gq/xml/XMLUtilities.java
+++ GQ_Android/src/com/qeevee/gq/xml/XMLUtilities.java
@@ -143,6 +143,45 @@ public class XMLUtilities {
 	}
 
 	/**
+	 * 
+	 * @param attributeName
+	 * @param defaultAsResourceID
+	 *            either NECESSARY_ATTRIBUTE, OPTIONAL_ATTRIBUTE or a valid
+	 *            resource ID which points to the default value for this
+	 *            attribute as a string resource.
+	 * @param xmlElement
+	 *            the XML element which defines the attribute, e.g. representing
+	 *            a mission or an item in game.xml
+	 * 
+	 * @return the corresponding attribute value as specified in the game.xml or
+	 *         null if the attribute is optional and not specified
+	 * @throws IllegalArgumentException
+	 *             if the attribute is necessary but not given in the game.xml
+	 */
+	public static Integer getIntegerAttribute(String attributeName,
+			int defaultAsResourceID, Element xmlElement) {
+		if (xmlElement == null
+				|| xmlElement.attributeValue(attributeName) == null)
+			if (defaultAsResourceID == NECESSARY_ATTRIBUTE) {
+				// attribute needed but not found => error in game.xml:
+				IllegalArgumentException e = new IllegalArgumentException(
+						"Necessary attribute \"" + attributeName
+								+ "\" missing. Rework game specification.");
+				Log.e(TAG, e.toString());
+				throw e;
+			} else if (defaultAsResourceID == OPTIONAL_ATTRIBUTE) {
+				// optional attribute not set in game.xml => return null:
+				return null;
+			} else
+				// attribute not set in game.xml but given as parameter => use
+				// referenced resource as default and return its value:
+				return GeoQuestApp.getInstance().getResources()
+						.getInteger(defaultAsResourceID);
+		else
+			return Integer.valueOf(xmlElement.attributeValue(attributeName));
+	}
+
+	/**
 	 * Loads the content (including html tags) of the given xml element into the
 	 * given webview.
 	 * 
