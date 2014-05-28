52c4d5cf502effbf8a42141e94c6b74c68a43ca0
Holger Muegge <muegge@acm.org>
2014-05-28 11:31:20 +0200

    zoomtoBoundingBox now implemented in own subclass of MapOSM

--- GQ_Android/src/com/qeevee/gq/loc/map/GQ_OSM_MapView.java
+++ GQ_Android/src/com/qeevee/gq/loc/map/GQ_OSM_MapView.java
@@ -4,19 +4,16 @@ import org.osmdroid.util.BoundingBoxE6;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-import com.qeevee.util.Util;
-
-import edu.bonn.mobilegaming.geoquest.GeoQuestApp;
-import edu.bonn.mobilegaming.geoquest.R;
-
 import android.content.Context;
 import android.util.AttributeSet;
 
 public class GQ_OSM_MapView extends MapView {
 
+	final private int MAX_LAT_SPAN = 360000000;
+	final private int MAX_LONG_SPAN = 170000000;
+
 	public GQ_OSM_MapView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
@@ -24,51 +21,30 @@ public class GQ_OSM_MapView extends MapView {
 	 * possible. Must be called after display layout is complete, or screen
 	 * dimensions are not known, and will always zoom to center of zoom level 0.
 	 * Suggestion: Check getScreenRect(null).getHeight() > 0
+	 * 
+	 * <p>
+	 * Call this method only after width and height of this map have already
+	 * been calculated! E.g. in an onGlobalLayoutListener within the activity.
 	 */
-	// public void zoomToBoundingBox(final BoundingBoxE6 boundingBox) {
-	// final BoundingBoxE6 currentBox = getBoundingBox(Util.getDisplayWidth()
-	// - 2 * R.dimen.margin, Util.getDisplayHeight() - 2
-	// * R.dimen.margin);
-	//
-	// // Calculated required zoom based on latitude span
-	// final double maxZoomLatitudeSpan = getZoomLevel(false) ==
-	// getMaxZoomLevel() ? currentBox
-	// .getLatitudeSpanE6() : currentBox.getLatitudeSpanE6()
-	// / Math.pow(2, getMaxZoomLevel() - getZoomLevel(false));
-	//
-	// if (maxZoomLatitudeSpan < 0.001) {
-	//
-	// }
-	// final double requiredLatitudeZoom = maxZoomLatitudeSpan < 0.001 ?
-	// getMaxZoomLevel()
-	// : getMaxZoomLevel()
-	// - Math.ceil(Math.log(boundingBox.getLatitudeSpanE6()
-	// / maxZoomLatitudeSpan)
-	// / Math.log(2));
-	//
-	// // Calculated required zoom based on longitude span
-	// final double maxZoomLongitudeSpan = getZoomLevel(false) ==
-	// getMaxZoomLevel() ? currentBox
-	// .getLongitudeSpanE6() : currentBox.getLongitudeSpanE6()
-	// / Math.pow(2, getMaxZoomLevel() - getZoomLevel(false));
-	//
-	// final double requiredLongitudeZoom = maxZoomLongitudeSpan < 0.001 ?
-	// getMaxZoomLevel()
-	// : getMaxZoomLevel()
-	// - Math.ceil(Math.log(boundingBox.getLongitudeSpanE6()
-	// / maxZoomLongitudeSpan)
-	// / Math.log(2));
-	//
-	// // Zoom to boundingBox center, at calculated maximum allowed zoom level
-	// int newZoomLevel = (int) (requiredLatitudeZoom < requiredLongitudeZoom ?
-	// requiredLatitudeZoom
-	// : requiredLongitudeZoom);
-	// getController().setZoom(newZoomLevel);
-	// GeoPoint newGP = new GeoPoint(boundingBox.getCenter().getLatitudeE6(),
-	// boundingBox.getCenter().getLongitudeE6());
-	// getController().setCenter(newGP);
-	// getController().animateTo(newGP);
-	//
-	// }
+	public void zoomToBoundingBox(final BoundingBoxE6 boundingBox) {
+		int maxZoomLevelLongitude = calculateMaxZoomLevel(getWidth(),
+				boundingBox.getLongitudeSpanE6(), MAX_LONG_SPAN);
+		int maxZoomLevelLatitude = calculateMaxZoomLevel(getHeight(),
+				boundingBox.getLatitudeSpanE6(), MAX_LAT_SPAN);
+		int zoomLevel = Math.min(
+				Math.min(maxZoomLevelLatitude, maxZoomLevelLongitude),
+				getMaxZoomLevel());
 
+		getController().setZoom(zoomLevel);
+		getController().setCenter(
+				new GeoPoint(boundingBox.getCenter().getLatitudeE6(),
+						boundingBox.getCenter().getLongitudeE6()));
+	}
+
+	private int calculateMaxZoomLevel(int availableLengthInPixel, int boxSpan,
+			int maxSpan) {
+		double length = (double) availableLengthInPixel;
+		return (int) Math.round(Math.log((maxSpan / boxSpan) * length)
+				/ Math.log(2)) - 8;
+	}
 }
