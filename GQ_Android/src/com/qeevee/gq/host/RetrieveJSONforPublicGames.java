40648b74070160f7e777282928f885859ba5a1e8
Holger Muegge <muegge@acm.org>
2014-10-21 09:14:36 +0200

    Extracted Task to retrieve public games into own class file

---
+++ GQ_Android/src/com/qeevee/gq/host/RetrieveJSONforPublicGames.java
@@ -0,0 +1,84 @@
+package com.qeevee.gq.host;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+class RetrieveJSONforPublicGames extends
+		AsyncTask<ConnectionStrategy, Void, String> {
+
+	protected String doInBackground(ConnectionStrategy... params) {
+		ConnectionStrategy connectionStrategy = params[0];
+		try {
+			return connect(DefaultConnectionStrategy.GQ_HOST_BASE_URL
+					+ "/json/" + connectionStrategy.getPortalID()
+					+ "/publicgames");
+		} catch (Exception e) {
+			Log.e(DefaultConnectionStrategy.TAG, e.getMessage());
+			return null;
+		}
+	}
+
+	private String connect(String url) {
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet(url);
+		HttpResponse response;
+		try {
+			response = httpclient.execute(httpget);
+			// Log.i(TAG,response.getStatusLine().toString());
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				InputStream instream = entity.getContent();
+				String result = convertStreamToString(instream);
+				instream.close();
+				return result;
+			}
+		} catch (ClientProtocolException e) {
+			Log.e(DefaultConnectionStrategy.TAG, e.getMessage());
+		} catch (IOException e) {
+			Log.e(DefaultConnectionStrategy.TAG, e.getMessage());
+		}
+		return null;
+	}
+
+	private String convertStreamToString(InputStream is) {
+		/*
+		 * To convert the InputStream to String we use the
+		 * BufferedReader.readLine() method. We iterate until the
+		 * BufferedReader return null which means there's no more data to
+		 * read. Each line will appended to a StringBuilder and returned as
+		 * String.
+		 */
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(is));
+		StringBuilder sb = new StringBuilder();
+
+		String line = null;
+		try {
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
+	}
+
+}
